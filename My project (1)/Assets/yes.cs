af24311e681173f236e88cebee6dc5a9a46c14c6
Artem-Unity-ru <Sigma-moment123@yandex.ru>
2025-03-09 19:26:43 +0300

    er

--- My project (1)/Assets/yes.cs
+++ My project (1)/Assets/yes.cs
@@ -8,27 +8,32 @@ public class yes : MonoBehaviour
     [Space]
 
     [Header("Активный доход")]
+    [SerializeField] ulong nuts = 1;
+    [SerializeField] ulong chocolate = 0;
+    [SerializeField] ulong orange = 0;
+    [SerializeField] ulong caramel = 0;
+    [Space]
+
+    [Header("Цены (активный доход)")]
+    [SerializeField] ulong chocolatePrice = 150;
+    [SerializeField] ulong nutsPrice = 1500;
+    [SerializeField] ulong orangePrise = 3000;
+    [SerializeField] ulong caramelPrise = 5000;
+    [Space]
+
+    [Header("Пассивный доход")]
     [SerializeField] uint babushka = 0;
     [SerializeField] ulong bakery = 0;
     [SerializeField] ulong customer = 0;
     [SerializeField] ulong shop = 0;
-    [Space]
 
-    [Header("Цены (активный доход)")]
-    [SerializeField] ulong babushkaPrice = 100;
-    [SerializeField] ulong bakeryPrice = 1500;
-    [SerializeField] ulong customerPrice = 3000;
-    [SerializeField] ulong shopPrice = 5000;
-    [Space]
-
-    [Header("Пассивный доход")]
-    [SerializeField] uint chocolate = 0;
-    [SerializeField] uint nuts = 0;
     [Space]
 
     [Header("Цены (пассивный доход)")]
-    [SerializeField] ulong chocolatePrice = 2000;
-    [SerializeField] ulong nutsPrice = 20000;
+    [SerializeField] ulong babushkaPrice = 1000;
+    [SerializeField] ulong bakeryPrice = 15000;
+    [SerializeField] ulong customerPrice = 30000;
+    [SerializeField] ulong shopPrice = 50000;
 
 
 
@@ -71,11 +76,90 @@ public class yes : MonoBehaviour
         {
             BuyNuts();
         }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           BuyOrange();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            BuyCaramel();
+        }
     }
 
     void ProcessClick()
     {
         coockies += babushka + bakery * 2 + customer * 3 + shop * 4;
+    }
+
+    void BuyNuts()
+    {
+        if (coockies >= nutsPrice)
+        {
+            coockies -= nutsPrice;
+            nutsPrice = (ulong)(nutsPrice * 1.18f);
+            nuts++;
+            print($"Поздравляем, Вы прокачали уровень орехов. Ваш уровень {nuts}");
+        }
+        else
+        {
+            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {nutsPrice - coockies} ");
+        }
+    }
+
+    void BuyChocolate()
+    {
+        if (coockies >= chocolatePrice)
+        {
+            coockies -= chocolatePrice;
+            chocolatePrice = (ulong)(chocolatePrice * 1.18f);
+            chocolate++;
+            print($"Поздравляем, Вы прокачали уровень шоколада. Ваш уровень {chocolate}");
+        }
+        else
+        {
+            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {chocolatePrice - coockies} ");
+        }
+    }
+
+
+    void BuyOrange()
+    {
+        if (coockies >= orangePrise)
+        {
+            coockies -= orangePrise;
+            orangePrise = (ulong)(chocolatePrice * 1.18f);
+            chocolate++;
+            print($"Поздравляем, Вы прокачали уровень шоколада. Ваш уровень {orange}");
+        }
+        else
+        {
+            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {orangePrise - coockies} ");
+        }
+    }  
+
+    void BuyCaramel()
+    {
+        if (coockies >= caramelPrise)
+        {
+            coockies -= caramelPrise;
+            caramelPrise = (ulong)(chocolatePrice * 1.18f);
+            chocolate++;
+            print($"Поздравляем, Вы прокачали уровень шоколада. Ваш уровень {caramel}");
+        }
+        else
+        {
+            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {caramelPrise - coockies} ");
+        }
+    }
+
+    IEnumerator AutoClick()
+    {
+        while (true)
+        {
+            coockies += chocolate + nuts * 2;
+            print(coockies);
+            yield return new WaitForSeconds(0.75f);
+        }
     }
 
     void BuyBabyshka()
@@ -135,46 +219,6 @@ public class yes : MonoBehaviour
         else
         {
             print($"К сожалению у Вас не хватает средств для покупки. Накопите еще {shopPrice - coockies}$");
-        }
-    }
-
-    IEnumerator AutoClick()
-    {
-        while (true)
-        {
-            coockies += chocolate + nuts * 2;
-            print(coockies);
-            yield return new WaitForSeconds(0.75f);
-        }
-    }
-
-    void BuyChocolate()
-    {
-        if (coockies >= chocolatePrice)
-        {
-            coockies -= chocolatePrice;
-            chocolatePrice = (ulong)(chocolatePrice * 1.18f);
-            chocolate++;
-            print($"Поздравляем, Вы прокачали уровень шоколада. Ваш уровень {chocolate}");
-        }
-        else
-        {
-            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {chocolatePrice - coockies} ");
-        }
-    }
-
-    void BuyNuts()
-    {
-        if (coockies >= nutsPrice)
-        {
-            coockies -= nutsPrice;
-            nutsPrice = (ulong)(nutsPrice * 1.18f);
-            nuts++;
-            print($"Поздравляем, Вы прокачали уровень орехов. Ваш уровень {nuts}");
-        }
-        else
-        {
-            print($"К сожалению у Вас не хватает средств для прокачки. Накопите еще {nutsPrice - coockies} ");
         }
     }
 }
